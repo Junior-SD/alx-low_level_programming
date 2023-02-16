91289a34c7d6c1e4d2d8cca27040450d7374268c
Junior-SD <juniormwadime01@gmail.com>
2023-02-16 12:04:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts function
- *Return: 0 (sucess)
- */
-int main (void)
-{       
-puts ("\" "Programming is like building a multilingual puzzle");
-return (0);
+*main-this is the main function
+*
+*Return; always 0
+*/
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
