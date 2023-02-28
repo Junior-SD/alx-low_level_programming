ed58d1652106cb810b69f50a747f622fc9114db0
Junior-SD <juniormwadime01@gmail.com>
2023-02-28 12:39:58 -0800

    files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* _strlen - prints every character of a string starting with the first line ..
+* @s: char type ..
+* Return: length of a string if success ..
+*/
+
+int _strlen(char *s)
+{
+int l, i = 0;
+
+while (s[i] != '\0')
+{
+i++;
+}
+l = i;
+return (l);
+}
+
+/**
+* puts2 - prints one char out of 2 of a string ..
+* @str: char type ..
+* Return: nothing .
+*/
+
+void puts2(char *str)
+{
+int i;
+int size = _strlen(str);
+
+for (i = 0; i < size; i += 2)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/*header*/
+
+/**
+* puts_half - get the length of a string ..
+* @str: char type ..
+* Return: 1 if success ..
+*/
+
+
+void puts_half(char *str)
+{
+int c = 0, i, d;
+
+for (i = 0; str[i]; i++)
+c++;
+if (c % 2 == 0)
+{
+i = c / 2;
+}
+else
+{
+d = (c - 1) / 2;
+i = c - d;
+}
+for (; i < c; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
